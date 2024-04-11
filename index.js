84953cdedc2c54bc95aeb13d96cb3b76c64797ff
VIKAS KUMAR SINGH <115570632+vikascse0509@users.noreply.github.com>
2024-04-11 21:23:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const apiUrl = "https://api.quotable.io/random"
+
+const quote = document.getElementById('quote')
+const author = document.getElementById('author')
+async function getQuote(url){
+    const response = await fetch(url)
+    var data = await response.json()
+    console.log(data)
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author
+}
+getQuote(apiUrl)
